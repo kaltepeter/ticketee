b2105fb4babd5d0190a415dea594b7f7fc65b0bc
Kayla Altepeter <kayla@kaylaaltepeter.com>
2013-08-17 16:22:27 -0500

    Fix signing up feature, take user to alternative confirmation page

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -11,7 +11,6 @@ end
 Given(/^I am signed in as them$/) do
 	steps(%Q(
 		Given I am on the homepage
-		When I follow "Sign in"
 		And I fill in "Email" with "#{@user.email}"
 		And I fill in "Password" with "password"
 		And I press "Sign in"
